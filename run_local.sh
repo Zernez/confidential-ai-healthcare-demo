0047cd33577edfc0c7f09102006327a18845d7d9
Zernez <fernando.pannullo@yahoo.it>
2025-10-08 09:58:11 +0200

    change attestator to sdk

--- run_local.sh
+++ run_local.sh
@@ -12,10 +12,10 @@ echo "Attivare l'environment Conda (rapids) prima dell'esecuzione."
 
 echo "Avvio main.py..."
 
-if [ -f "docker/requirements.txt" ]; then
-    echo "Installo dipendenze da requirements.txt..."
-    python3 -m pip install --upgrade pip
-    python3 -m pip install -r docker/requirements.txt
-fi
+# if [ -f "docker/requirements.txt" ]; then
+#     echo "Installo dipendenze da requirements.txt..."
+#     python3 -m pip install --upgrade pip
+#     python3 -m pip install -r docker/requirements.txt
+# fi
 
 python3 main.py
