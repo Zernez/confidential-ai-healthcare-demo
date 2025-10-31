7aa50ca28c8887872d6a3e4bdc9a75ffd2262fab
Zernez <fernando.pannullo@yahoo.it>
2025-10-31 20:18:11 +0100

    fix script conda

--- setup_conda_rapids.sh
+++ setup_conda_rapids.sh
@@ -31,9 +31,9 @@ conda install -y -c rapidsai -c conda-forge -c nvidia cubinlinker ptxcompiler
 conda install -y -c rapidsai -c conda-forge -c nvidia cudf=24.04 cuml=24.04
 
 # Installa requirements.txt con pip
-if [ -f "docker/requirements.txt" ]; then
-    echo "Installazione dipendenze da requirements.txt..."
-    pip install -r docker/requirements.txt
-fi
+# if [ -f "docker/requirements.txt" ]; then
+#     echo "Installazione dipendenze da requirements.txt..."
+#     pip install -r docker/requirements.txt
+# fi
 
 echo "Ambiente rapids pronto! Ora puoi eseguire run_local.sh."
